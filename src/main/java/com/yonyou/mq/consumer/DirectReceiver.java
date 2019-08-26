e1a488b33026c8b1dad34e501b478bd9cebe8517
sirsoda <1563004690@qq.com>
2019-08-27 00:24:03 +0800

    rabbitmq

---
+++ src/main/java/com/yonyou/mq/consumer/DirectReceiver.java
@@ -0,0 +1,12 @@
+package com.yonyou.mq.consumer;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DirectReceiver {
+    @RabbitListener(queues = "hello-queue")
+    public void handler1(String msg) {
+        System.out.println("DirectReceiver:" + msg);
+    }
+}
